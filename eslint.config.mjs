2a99493d7d98bc5f9766c1a723ad286905982b3b
24f1000754 <24f1000754@ds.study.iitm.ac.in>
2026-06-01 22:38:40 +0530

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,2 +1,20 @@
-const eslintConfig = [];
+import js from "@eslint/js";
+
+const eslintConfig = [
+  {
+    ...js.configs.recommended,
+    files: ["**/*.{js,jsx}"],
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    rules: {},
+  },
+];
+
 export default eslintConfig;
